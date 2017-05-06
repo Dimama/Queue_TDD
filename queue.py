5cfd02c45761eff907324da2e91e4d06f0a56aeb
Dimama <citrus_m@mail.ru>
2017-05-06 15:58:31 +0300

    Realization of Queue was added. All tests are done.

--- queue.py
+++ queue.py
@@ -1,5 +1,6 @@
 from stack import Stack
 
+
 class Queue(object):
 
     def __init__(self, items=[]):
@@ -7,22 +8,39 @@ class Queue(object):
         self.out_stack = Stack()
 
     def enqueue(self, item):
-        pass
+        self.in_stack.push(item)
 
     def dequeue(self):
-        pass
+        if self.out_stack.is_empty():
+            while not (self.in_stack.is_empty()):
+                self.out_stack.push(self.in_stack.pop())
+        return self.out_stack.pop()
 
     def maximum(self):
-        pass
+        if self.is_empty():
+            return None
+        if self.in_stack.is_empty() or self.out_stack.is_empty():
+            if self.in_stack.is_empty():
+                return self.out_stack.top().max_val
+            return self.in_stack.top().max_val
+        else:
+            return max(self.out_stack.top().max_val, self.in_stack.top().max_val)
 
     def minimum(self): 
-        pass
+        if self.is_empty():
+            return None
+        if self.in_stack.is_empty() or self.out_stack.is_empty():
+            if self.in_stack.is_empty():
+                return self.out_stack.top().min_val
+            return self.in_stack.top().min_val
+        else:
+            return min(self.out_stack.top().min_val, self.in_stack.top().min_val)
 
     def size(self):
-        pass
+        return len(self.out_stack.items) + len(self.in_stack.items)
 
     def is_empty(self):
-        pass
+        return self.out_stack.is_empty() and self.in_stack.is_empty()
 
     def get_items(self):
         res = []
